9ef97315a2ae7e4f2ec7b3a1460661e1dc6a84d0
Kevin Pham <keevan.pham@gmail.com>
2021-10-02 14:20:41 +1000

    Add support reading victims from the environment
    - Used as a fallback when victims.yml file does not exist

--- victims.go
+++ victims.go
@@ -2,8 +2,10 @@
 package main
 
 import (
+	"os"
 	"log"
 	"io/ioutil"
+	"strings"
 	"gopkg.in/yaml.v2"
   )
 
@@ -21,15 +23,28 @@ func getVictims() []string {
 
 	// Read File
 	yamlFile, err := ioutil.ReadFile(victimsFile)
-	if err != nil {
-		log.Fatalf("[ERROR] %v", err)
+	if err == nil {
+
+		// Unmarshall Creds
+		err = yaml.Unmarshal(yamlFile, v)
+		if err != nil {
+			log.Fatalf("[ERROR] %v", err)
+		}
+
+	} else {
+
+		// Fall back to using environment variables if no victims.yml file exists
+		envVictims := os.Getenv("VICTIMS")
+		if len(envVictims) > 0 {
+			v.Victims = strings.Split(envVictims, ",")
+		} else {
+			// Fails as if no victims were provided (file or env)
+			log.Fatalf("[ERROR] %v", err)
+		}
+
 	}
 
-	// Unmarshall Creds
-	err = yaml.Unmarshal(yamlFile, v)
-	if err != nil {
-		log.Fatalf("[ERROR] %v", err)
-	}
+
 
 	return v.Victims
 }
